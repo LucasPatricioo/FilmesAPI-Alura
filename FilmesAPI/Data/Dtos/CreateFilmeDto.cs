ef48e4cd59857144f827374662b6035a78c780f4
Lucas Patricio <lucas_patricio@outlook.com.br>
2023-04-03 00:17:27 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class CreateFilmeDto
+{
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genêro do filme é obrigatório")]
+    [StringLength(50, ErrorMessage = "O tamanho do campo genêro não pode ser maior do que 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(60, 600, ErrorMessage = "A duração deve ser entre 60 a 600 minutos")]
+    public int Duracao { get; set; }
+}
+
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; internal set; }
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genêro do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do campo genêro não pode ser maior do que 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(60, 600, ErrorMessage = "A duração deve ser entre 60 a 600 minutos")]
+    public int Duracao { get; set; }
+}
+
